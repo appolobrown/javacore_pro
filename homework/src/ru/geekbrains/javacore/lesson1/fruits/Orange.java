8cf596b47c9236ff2f4db540e8fa1925d2d0a98c
Evgeny Anonov <capevery@gmail.com>
2020-11-11 21:47:38 +0300

    home task

---
+++ homework/src/ru/geekbrains/javacore/lesson1/fruits/Orange.java
@@ -0,0 +1,10 @@
+package ru.geekbrains.javacore.lesson1.fruits;
+
+public class Orange extends Fruit {
+
+    private static final float ORANGE_WEIGHT = 1.5f;
+
+    public Orange() {
+        super(ORANGE_WEIGHT);
+    }
+}
