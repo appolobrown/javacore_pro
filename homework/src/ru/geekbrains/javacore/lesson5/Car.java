4ffac52700003bce18fe1df932441981ed1bb612
Evgeny Anonov <capevery@gmail.com>
2020-11-26 12:00:44 +0300

    homework5

--- homework/src/ru/geekbrains/javacore/lesson5/Car.java
+++ homework/src/ru/geekbrains/javacore/lesson5/Car.java
@@ -1,7 +1,13 @@
 package ru.geekbrains.javacore.lesson5;
 
+import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class Car implements Runnable {
     private static int CARS_COUNT;
+    public static final AtomicInteger FINISH_COUNTER = new AtomicInteger(0);
+    public static final AtomicBoolean IS_RACE_BEGIN = new AtomicBoolean(false);
 
     static {
         CARS_COUNT = 0;
@@ -10,6 +16,7 @@ public class Car implements Runnable {
     private final Race race;
     private final int speed;
     private final String name;
+    private final CyclicBarrier cyclicBarrier;
 
     public String getName() {
         return name;
@@ -19,11 +26,12 @@ public class Car implements Runnable {
         return speed;
     }
 
-    public Car(Race race, int speed) {
+    public Car(Race race, int speed, CyclicBarrier cyclicBarrier) {
         this.race = race;
         this.speed = speed;
         CARS_COUNT++;
         this.name = "Участник #" + CARS_COUNT;
+        this.cyclicBarrier = cyclicBarrier;
     }
 
     @Override
@@ -32,11 +40,16 @@ public class Car implements Runnable {
             System.out.println(this.name + " готовится");
             Thread.sleep(500 + (int) (Math.random() * 800));
             System.out.println(this.name + " готов");
+            cyclicBarrier.await();
+            IS_RACE_BEGIN.set(true);
+
         } catch (Exception e) {
             e.printStackTrace();
         }
         for (int i = 0; i < race.getStages().size(); i++) {
             race.getStages().get(i).go(this);
         }
+        System.out.println(this.name + " финишировал " + FINISH_COUNTER.incrementAndGet());
     }
+
 }
